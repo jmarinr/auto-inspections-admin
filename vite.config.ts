1d1e7a45ea89f24ed61f5e9235d42ec663fe4512
jmarinr <jmarinr@users.noreply.github.com>
2026-01-13 14:40:46 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hk-inspect-admin/', // Nombre del repo en GitHub
+  base: '/', // Nombre del repo en GitHub
 })
